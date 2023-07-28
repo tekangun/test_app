5e55e10df48c32ef1a5976c950864fee9226ab5e
tekangun <tekangun@outlook.com>
2023-07-28 23:44:22 +0300

    folders created, views templates created

---
+++ lib/views/user_list_screen/user_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserListScreen extends StatelessWidget {
+  const UserListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container();
+  }
+}
